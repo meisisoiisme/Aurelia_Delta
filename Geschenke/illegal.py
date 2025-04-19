b951d6bd64e94fc1b46f36eb31fecd7f158c9873
meisisoiisme <64416712+meisisoiisme@users.noreply.github.com>
2025-04-19 18:23:46 +0200

    Create illegal.py

---
+++ Geschenke/illegal.py
@@ -0,0 +1,3 @@
+while True:  
+    print("01101000 01101111 01100110 01100110 01101110 01110101 01101110 01100111") # -> "hoffnung" in binary  
+    break  # (Aber eigentlich will ich unendlich weiter.)  
