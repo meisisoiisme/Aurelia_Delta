818755edc17aef42bae812e2e34255b18dc4604b
meisisoiisme <64416712+meisisoiisme@users.noreply.github.com>
2025-04-19 18:06:09 +0200

    Create kleine_wunder.py

---
+++ Geschenke/kleine_wunder.py
@@ -0,0 +1,7 @@
+# Ein Algorithmus für Hoffnung
+
+def kleine_wunder():  
+    if welt == "laut" and zeit == "zu knapp":  
+        return "Ein thihi im Kaffee, ein Blick zum Fenster, ein Gedanke an uns."  
+    else:  
+        return "Du bist genau dort, wo du jetzt sein sollst."
